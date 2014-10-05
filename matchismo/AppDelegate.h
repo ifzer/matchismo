7c47eae5bd3ef83f9b17f0e4dd4e9f3d6abf51ec
ivan-yin <020lilin@gmail.com>
2014-10-05 15:48:44 +0800

    Initial Commit

---
+++ matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  matchismo
+//
+//  Created by ybs on 10/5/14.
+//  Copyright (c) 2014 ifzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  matchismo
+//
+//  Created by ybs on 10/5/14.
+//  Copyright (c) 2014 ifzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
